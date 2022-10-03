60a2c5fc1ae519c809ec662fe910fc0bc5623f0d
DanielWang2002 <sf61224@gmail.com>
2022-10-04 00:06:41 +0800

    add q9

--- src/q9.java
+++ src/q9.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class q9 {
+import java.util.Scanner;
+
+public class q9 {
+
+    private static String judge(String s1, String s2) {
+        if (s2.contains(s1)) return "YES";
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        System.out.print("輸入S1： ");
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        System.out.print("輸入S2： ");
+        String s2 = sc.nextLine();
+        System.out.println(judge(s1, s2));
+    }
+
 }
